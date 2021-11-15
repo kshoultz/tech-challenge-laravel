73865c8cc1f043b3f867a67c8f4a4bf81f0cc16e
Kevin <jedi.kevo@gmail.com>
2021-11-15 16:34:30 -0600

    Update index.php
    revert

--- public/index.php
+++ public/index.php
@@ -51,7 +51,5 @@ $kernel = $app->make(Kernel::class);
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
-header('Access-Control-Allow-Origin: *'); 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'); 
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 $kernel->terminate($request, $response);
