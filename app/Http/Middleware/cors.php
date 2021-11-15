9b5c8a567ca10a90c2c844a854cafc9b71353842
Kevin <jedi.kevo@gmail.com>
2021-11-15 09:00:05 -0600

    Update cors.php
    specific domain

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -17,7 +17,7 @@ class cors
     public function handle($request, Closure $next) {
    
         return $next($request)
-          ->header('Access-Control-Allow-Origin', '*')
+          ->header('Access-Control-Allow-Origin', 'https://tech-challenge-node-next-beta.vercel.app')
           ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
           ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
           ->header('Access-Control-Allow-Credentials',' true');
